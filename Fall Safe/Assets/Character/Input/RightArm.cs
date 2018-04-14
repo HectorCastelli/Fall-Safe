e32f9536c4da452122edbb0608d8e3e827968253
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2018-04-14 11:15:15 -0400

    Removed Rewired, Added cinemachine, started working with the new part-by-part input.

---
+++ Fall Safe/Assets/Character/Input/RightArm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightArm : MonoBehaviour {
+
+    public bool _isEnabled = false;
+
+    private Vector3 _oldMousePosition;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_isEnabled) return;
+        if (Input.GetMouseButton(0))
+        {
+            //Get Mouse Delta
+            Vector3 mouseDelta = Input.mousePosition - _oldMousePosition;
+            _oldMousePosition = Input.mousePosition;
+            //Manipulate Right Arm
+            //X is shoulder
+            //Y is elbow
+            this.GetComponent<Rigidbody>().AddRelativeTorque(0, mouseDelta.x, mouseDelta.y, ForceMode.Impulse);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isEnabled = false;
+            //Destroy(this.GetComponent<CharacterJoint>());
+            //Destroy(this.GetComponent<Rigidbody>());
+            this.GetComponent<Rigidbody>().isKinematic = true;
+        }
+	}
+}
