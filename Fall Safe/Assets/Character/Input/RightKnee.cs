6576e196ebb01c5e3efd4f9a58b8f5f4f4c1a211
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2018-04-14 11:35:34 -0400

    Made full controll of each body joint.

---
+++ Fall Safe/Assets/Character/Input/RightKnee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightKnee : MonoBehaviour
+{
+    public bool _isEnabled = false;
+
+    private Vector3 _oldMousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isEnabled) return;
+        if (Input.GetMouseButton(0))
+        {
+            //Get Mouse Delta
+            Vector3 mouseDelta = Input.mousePosition - _oldMousePosition;
+            _oldMousePosition = Input.mousePosition;
+            //Manipulate Right Arm
+            //X is shoulder
+            //Y is elbow
+            this.GetComponent<Rigidbody>().AddRelativeTorque(0, mouseDelta.y, -mouseDelta.x, ForceMode.Impulse);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isEnabled = false;
+            //Destroy(this.GetComponent<CharacterJoint>());
+            //Destroy(this.GetComponent<Rigidbody>());
+            this.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
