0f3ce45db9723fbc6ab4f854b340c996d7be97a6
Luke Gillen <gillenluke53@yahoo.com>
2018-04-15 11:30:18 -0400

    pushing test scene

---
+++ Fall Safe/Assets/Scripts/SliderInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderInfo : MonoBehaviour
+{
+    [SerializeField] Image HealthBarImage;
+    bool hasPressed = false;
+    
+    // Use this for initialization
+    void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+        HealthBarImage.fillAmount = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        HealthBarImage.fillAmount -= (Time.fixedDeltaTime/5);
+        if (Input.GetMouseButtonDown(0)&&!hasPressed)
+        {
+            StartCoroutine(AdvanceBar());
+        }
+    }
+    public float GetSliderValue
+    {
+        get { return HealthBarImage.fillAmount; }
+       
+    }
+  IEnumerator  AdvanceBar()
+    {
+        hasPressed = true;
+        yield return new WaitForSeconds(.1f);
+        Debug.Log("Current Fill Amount = "+ HealthBarImage.fillAmount);
+    }
+}
